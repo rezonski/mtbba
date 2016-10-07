1a72e46c4350cb9e3331bb6c8c5408895560aad1
mekee <mersad.pasic@gmail.com>
2016-10-07 18:00:49 +0200

    Add mnt sleector

--- glu2demo/glu2-demo-app/src/components/forms/AddNewTrail.js
+++ glu2demo/glu2-demo-app/src/components/forms/AddNewTrail.js
@@ -18,16 +18,27 @@ class AddNewTrail extends BasePage {
         this.state = {
             open: false,
             message: '',
-            type: null,
             data: {},
-            mountainID: null,
+            trailName: '',
+            trailDesc: '',
+            mountainIDs: [],
             trailTypeID: null,
         };
+
+        this.form = {
+            trailName: '',
+            trailDesc: '',
+            mountainIDs: [],
+            trailTypeID: null,
+        };
+
+        // this.mounted = false;
 
         this.onCloseEvent = this.handleClose.bind(this);
         this.onSaveAddedTrail = this.saveAddedTrail.bind(this);
         this.onUploadDoneEvent = this.onUploadDone.bind(this);
-        this.eventChangeMountain = this.onFormChangeMountain.bind(this);
+        this.onTrailNameChangedEvent = this.onTrailNameChanged.bind(this);
+        this.onTrailDescChangedEvent = this.onTrailDescChanged.bind(this);
         this.eventChangeTrailType = this.onFormChangeTrailType.bind(this);
 
         this.bindGluBusEvents({
@@ -37,8 +48,13 @@ class AddNewTrail extends BasePage {
     }
 
     componentDidMount() {
+        // this.mounted = true;
         this.emit(Enum.MapEvents.RETRIEVE_INITIAL_DATA_SETUP);
     }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     // return nextProps !== this.props || nextState.data !== this.state.data;
+    // }
 
     componentWillUnmount() {
         this.unbindGluBusEvents();
@@ -60,16 +76,42 @@ class AddNewTrail extends BasePage {
         });
     }
 
-    onFormChangeMountain(event, index, value) {
-        this.setState({
-            mountainID: value,
-        });
+    onTrailNameChanged(event) {
+        this.form.trailName = event.target.value;
+        // this.setState({
+        //     trailName: event.target.value,
+        // });
+    }
+
+    onTrailDescChanged(event) {
+        this.form.trailDesc = event.target.value;
+        // this.setState({
+        //     trailDesc: event.target.value,
+        // });
     }
 
     onFormChangeTrailType(event, index, value) {
-        this.setState({
-            trailTypeID: value,
-        });
+        this.form.trailTypeID = value;
+        // this.setState({
+        //     trailTypeID: value,
+        // });
+    }
+
+    onFormChangeMountain(mntID, event, isChecked) {
+        let temp = this.state.mountainIDs;
+        if (isChecked) {
+            if (temp.indexOf(mntID) === -1) {
+                temp.push(mntID);
+            }
+        } else {
+            if (temp.indexOf(mntID) > -1) {
+                temp.splice(temp.indexOf(mntID), 1);
+            }
+        }
+        this.form.mountainIDs = temp;
+        // this.setState({
+        //     mountainIDs: temp,
+        // });
     }
 
     render() {
@@ -101,7 +143,8 @@ class AddNewTrail extends BasePage {
           />,
         ];
 
-        console.info(this.state.data);
+        // console.info(this.state.data);
+        // console.info(this.state.mountainIDs);
 
         let mountainItems = [];
         let trailTypes = [];
@@ -113,7 +156,7 @@ class AddNewTrail extends BasePage {
                 this.state.data.mountains.forEach((mountain, mIndex ) => {
                     if (mountain.id_parent === country.id) {
                         // mountainItems.push(<MenuItem key={ 'mountain' + mIndex } value={mountain.id} primaryText={mountain.name + ' / ' + mountain.region + ' (' + mountain.total + ')'} />);
-                        mountainItems.push(<Checkbox key={ 'mountain' + mIndex } label={mountain.name + ' / ' + mountain.region + ' (' + mountain.total + ')'} style={styles.checkbox} />);
+                        mountainItems.push(<Checkbox key={ 'mountain' + mIndex } checked={this.mntChecked(mountain.id)} onCheck={this.onFormChangeMountain.bind(this, mountain.id)} label={mountain.name + ' / ' + mountain.region + ' (' + mountain.total + ')'} style={styles.checkbox} />);
                     }
                 });
                 mountainItems.push(<Divider key={ 'country-divider' + cIndex }/>);
@@ -131,11 +174,11 @@ class AddNewTrail extends BasePage {
                             <br />
                             <input type="file" onChange={this.onUploadDoneEvent}/>
                             <br />
-                            <TextField fullWidth={true} hintText="E.g. 'Sarajevo-Nahorevo-Skakavac'" floatingLabelText="Trail name"/>
+                            <TextField onChange={this.onTrailNameChangedEvent} value={this.form.trailName} fullWidth={true} hintText="E.g. 'Sarajevo-Nahorevo-Skakavac'" floatingLabelText="Trail name"/>
                             <br />
-                            <TextField fullWidth={true} hintText="Between 200 and 400 character" floatingLabelText="Trail description" multiLine={true} rows={4}/>
+                            <TextField onChange={this.onTrailDescChangedEvent} value={this.form.trailDesc} fullWidth={true} hintText="Between 200 and 400 character" floatingLabelText="Trail description" multiLine={true} rows={4}/>
                             <br />
-                            <SelectField value={this.state.trailTypeID} onChange={this.eventChangeTrailType} floatingLabelText="Choose trail type" floatingLabelFixed={true} hintText="Choose only one from list" maxHeight={200} autoWidth={true}>{trailTypes}</SelectField>
+                            <SelectField onChange={this.eventChangeTrailType} value={this.form.trailTypeID} floatingLabelText="Choose trail type" floatingLabelFixed={true} hintText="Choose only one from list" maxHeight={200} autoWidth={true}>{trailTypes}</SelectField>
                         </div>
                         <div className="flex-element column narower">
                             <div className="list-container">
@@ -154,6 +197,13 @@ class AddNewTrail extends BasePage {
 
     saveAddedTrail() {
         console.info('spasi');
+    }
+
+    mntChecked(mntID) {
+        if (this.form.mountainIDs.indexOf(mntID) > -1) {
+            return true;
+        }
+        return false;
     }
 }
 
