3294a0f253c5e17dbabb31f3989b223195257f62
Mersad Pasic <mersad.pasic@gmail.com>
2016-12-18 23:08:00 +0100

    Try to run static

--- examples/setfilefromurl.php
+++ examples/setfilefromurl.php
@@ -1,8 +1,8 @@
 <?php
-    if (isset($_GET['getURL']) && isset($_GET['setURL'])) {  
-        $getURL = $_GET['getURL'];
-        $setURL = $_GET['setURL'];
-        $getURL = "http://www.theonlytutorials.com/images/logo.png";
+    if (isset($_GET['polyline']) && isset($_GET['fileName'])) {  
+        $getURL = 'https://api.mapbox.com/v4/mapbox.streets/path-5+f44-0.5+f44-0.2('.$_GET['polyline'].')/18.501629601708554,43.843932094371844,10/500x300.png?access_token=pk.eyJ1IjoibWVyc2FkcGFzaWMiLCJhIjoiY2lqenc1ZnpkMDA2N3ZrbHo4MzQ2Z2YzZyJ9.TIDhGaRGIYtw9_f_Yb3Ptg';
+        echo $getURL;
+        $setURL = 'upload/'.$_GET['fileName'].'png';
         $file = file_get_contents($getURL); 
         if (file_put_contents($setURL, $file) === false) {
             echo "# Error: Unable to upload"; 
