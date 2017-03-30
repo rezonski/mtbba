76f504145510580032955e4539467dd692213697
Mersad Pasic <mersad.pasic@gmail.com>
2017-03-30 22:43:53 +0200

    Progress

--- glu2demo/glu2-demo-app/src/components/map/MapEditControl.js
+++ glu2demo/glu2-demo-app/src/components/map/MapEditControl.js
@@ -6,37 +6,47 @@ import Enum from '/enums/Enum';
 class MapEditControl {
     onAdd(map) {
         this._map = map;
+        window.listenClick = false;
         window.clickedPoint = {};
-        window.pressedKeyCode = '';
         window.listenKeyboard = false;
+        window.pressedKeyCode = '';
         this._container = document.createElement('div');
         this._container.className = 'mapboxgl-ctrl-group mapboxgl-ctrl';
+
+        // POinter
+        this._pointerButton = document.createElement('button');
+        this._pointerButton.className = 'mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw-pointer';
+        this._pointerButton.title = 'Toggle on/off temp point on map';
+        this._pointerButton.addEventListener('click', this.onTogglePointer);
+        this._container.appendChild(this._pointerButton);
 
         // Splitter
         this._trailSpritterButton = document.createElement('button');
         this._trailSpritterButton.className = 'mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw-split';
         this._trailSpritterButton.title = 'Split trail line string near the selected point on map';
-        this._trailSpritterButton.addEventListener('click', this.onSplitterEvent);
+        this._trailSpritterButton.addEventListener('click', this.onSplitter);
         this._container.appendChild(this._trailSpritterButton);
 
         // Add point
         this._addWaypointButton = document.createElement('button');
         this._addWaypointButton.className = 'mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw-add-point';
         this._addWaypointButton.title = 'Add new waypoint clicking on map';
-        this._addWaypointButton.addEventListener('click', this.onAddWaypointEvent);
+        this._addWaypointButton.addEventListener('click', this.onAddWaypoint);
         this._container.appendChild(this._addWaypointButton);
 
         // Add terrain switch
         this._addTerrainSwitchButton = document.createElement('button');
         this._addTerrainSwitchButton.className = 'mapbox-gl-draw_ctrl-draw-btn mapbox-gl-draw-add-terrain';
         this._addTerrainSwitchButton.title = 'Add new point where surface changes';
-        this._addTerrainSwitchButton.addEventListener('click', this.onAddTerrainSwitchEvent);
+        this._addTerrainSwitchButton.addEventListener('click', this.onAddTerrainSwitch);
         this._container.appendChild(this._addTerrainSwitchButton);
 
         window.leftmap.on('click', e => {
-            window.clickedPoint = JSON.parse(JSON.stringify(e.lngLat));
-            console.log('window.clickedPoint = ' + JSON.stringify(window.clickedPoint));
-            window.leftmap.fire('displayTempPoint', { position: window.clickedPoint });
+            if (window.listenClick) {
+                window.clickedPoint = JSON.parse(JSON.stringify(e.lngLat));
+                console.log('window.clickedPoint = ' + JSON.stringify(window.clickedPoint));
+                window.leftmap.fire('displayTempPoint', { position: window.clickedPoint });
+            }
         });
 
         window.addEventListener('keydown', keyEvent => {
@@ -59,19 +69,24 @@ class MapEditControl {
         this._map = undefined;
     }
 
+    onTogglePointer() {
+        window.leftmap.fire('saveEditedPath');
+        window.listenClick = !window.listenClick;
+    }
+
     onSplitter() {
         window.leftmap.fire('saveEditedPath');
         console.log('onSplitterEvent(' + JSON.stringify(window.clickedPoint) + ')');
         window.leftmap.fire('lineSlice', { position: window.clickedPoint });
     }
 
-    onAddWaypointEvent() {
+    onAddWaypoint() {
         window.leftmap.fire('saveEditedPath');
-        console.log('onAddWaypointEvent(' + JSON.stringify(window.clickedPoint) + ')');
+        console.log('onAddWaypoint(' + JSON.stringify(window.clickedPoint) + ')');
         GLU.bus.emit(Enum.DataEvents.ADD_NEW_WAYPOINT, { position: window.clickedPoint });
     }
 
-    onAddTerrainSwitchEvent() {
+    onAddTerrainSwitch() {
         window.listenKeyboard = true;
         window.leftmap.fire('saveEditedPath');
         GLU.bus.emit(MessageEvents.LONGER_INFO_MESSAGE, Lang.msg('keypress4surfaceType'));
