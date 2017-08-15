bf2be4a20cd10e8d66b452581683644ccd254934
mekee <mersad.pasic@gmail.com>
2017-08-15 17:45:25 +0200

    fix

--- examples/js/48chart.js
+++ examples/js/48chart.js
@@ -6,24 +6,24 @@ function setElevationProfile(chartContainer,pathLine,wayPoints,surfaceCollection
     // var plotlines = [];
     // var plotline = {};
 
-    // pathLine.forEach(function(element,index){
+    pathLine.forEach(function(element,index){
 
-    //     if (index > 0) {
-    //         currentPart = getDistance(pathLine[index-1], element) 
-    //         odometer.push(Math.round((odometer[index-1] + currentPart)*100000)/100000);
-    //     }
+        if (index > 0) {
+            currentPart = getDistance(pathLine[index-1], element) 
+            odometer.push(Math.round((odometer[index-1] + currentPart)*100000)/100000);
+        }
         
-    //     // dataset.push(Math.round(element[2]*100000)/100000);
-    //     // dataset.push(element[2]);
-    //     // dataset.push(odometer[index], element[2]);
-    //     if (element[2] > 0) {
-    //         // console.log(odometer[index]);
-    //         dataset.push({x: odometer[index], y: element[2], segmentColor: getSegmentColor(getSegmentName(odometer[index], surfaceCollection)).rgb});
-    //         // dataset.push({y: element[2], segmentColor: getSegmentColor(getSegmentName(odometer[index], surfaceCollection))});
-    //         // dataset.push([odometer[index], element[2], getSegmentColor(getSegmentName(odometer[index], surfaceCollection))]);
-    //         // dataset.push([odometer[index], element[2]]);
-    //     }
-    // });
+        // dataset.push(Math.round(element[2]*100000)/100000);
+        // dataset.push(element[2]);
+        // dataset.push(odometer[index], element[2]);
+        if (element[2] > 0) {
+            // console.log(odometer[index]);
+            dataset.push({x: odometer[index], y: element[2], segmentColor: getSegmentColor(getSegmentName(odometer[index], surfaceCollection)).rgb});
+            // dataset.push({y: element[2], segmentColor: getSegmentColor(getSegmentName(odometer[index], surfaceCollection))});
+            // dataset.push([odometer[index], element[2], getSegmentColor(getSegmentName(odometer[index], surfaceCollection))]);
+            // dataset.push([odometer[index], element[2]]);
+        }
+    });
 
 
     // wayPoints.forEach(function(waypoint){
@@ -49,105 +49,105 @@ function setElevationProfile(chartContainer,pathLine,wayPoints,surfaceCollection
     document.getElementById(chartContainer).style.display = "block";
 
 
-    // $(function () {
-    //     $('#' + chartContainer).highcharts({
-    //         chart: {
-    //             renderTo: chartContainer,
-    //             type: 'coloredarea',
-    //             zoomType: 'xy',
-    //             borderWidth: 5,
-    //             borderColor: '#e8eaeb',
-    //             borderRadius: 0,
-    //             backgroundColor: '#f7f7f7'
-    //         },
-    //         title: {
-    //             text: rawTrailName.toString()
-    //         },
-    //         legend: {
-    //             enabled: false
-    //         },
-    //         xAxis: {
-    //             type: 'integer',
-    //             // categories: odometer,
-    //             // tickInterval: 2,
-    //             title: {
-    //                 text: 'Predjeni put [km]'
-    //             },
-    //             plotLines: plotlines
-    //         },
-    //         yAxis: {
-    //             title: {
-    //                 text: 'Nadmorska visina [m]'
-    //             },
-    //             plotLines: [{
-    //                 value: 0,
-    //                 width: 1,
-    //                 color: '#808080'
-    //             }],
-    //             min: 0,
-    //             max: 2000
-    //         },
-    //         tooltip: {
-    //             // shared: true,
-    //             // valueSuffix: ' m',
-    //             // crosshairs: true
+    $(function () {
+        $('#' + chartContainer).highcharts({
+            chart: {
+                renderTo: chartContainer,
+                type: 'coloredarea',
+                zoomType: 'xy',
+                borderWidth: 5,
+                borderColor: '#e8eaeb',
+                borderRadius: 0,
+                backgroundColor: '#f7f7f7'
+            },
+            title: {
+                text: rawTrailName.toString()
+            },
+            legend: {
+                enabled: false
+            },
+            xAxis: {
+                type: 'integer',
+                // categories: odometer,
+                // tickInterval: 2,
+                title: {
+                    text: 'Predjeni put [km]'
+                },
+                plotLines: plotlines
+            },
+            yAxis: {
+                title: {
+                    text: 'Nadmorska visina [m]'
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 1,
+                    color: '#808080'
+                }],
+                min: 0,
+                max: 2000
+            },
+            tooltip: {
+                // shared: true,
+                // valueSuffix: ' m',
+                // crosshairs: true
 
-    //             shared: true,
-    //             useHTML: true,
-    //             headerFormat: '<b>{point.key} km</b><br>',
-    //             pointFormat: '{point.y} mnv',
-    //             valueDecimals: 2
-    //         },
-    //         credits: {
-    //             enabled: false
-    //         },
-    //         plotOptions: {
-    //             area: {
-    //                 fillOpacity: 0.1,
-    //                 connectNulls : true,
-    //                 marker: {
-    //                     enabled: false,
-    //                     symbol: 'circle',
-    //                     radius: 2,
-    //                     states: {
-    //                         hover: {
-    //                             enabled: true
-    //                         }
-    //                     }
-    //                 }
-    //             },
-    //             series: {
-    //                 cursor: 'pointer',
-    //                 point: {
-    //                     events: {
-    //                         click: function () {
-    //                             // console.log('Click: ' + (Math.round(this.x * 100) / 100));
-    //                             document.getElementById("sastavodometar").value = (Math.round(this.x * 100) / 100);
-    //                             selectedPointOnTrail = pathLineMasterd[this.index];
-    //                         },
-    //                         mouseOver: function () {
-    //                             // console.log('Over: ' + (Math.round(this.x * 100) / 100));
-    //                             // console.log('Coordinates: ' + pathLine[this.index][0] + ' , ' + pathLine[this.index][1]);
-    //                             // currentLocSourceObject.geometry.coordinates = [pathLine[this.index][0],pathLine[this.index][1]];
-    //                             animateMarker([pathLine[this.index][0], pathLine[this.index][1]]);
-    //                             // if (Number.isInteger(this.index) && pathLine[this.index][0] !== undefined && pathLine[this.index][1] !== undefined) {
-    //                             // } else {
-    //                             //     console.log('NO INT');
-    //                             // }
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         },
-    //         series: [{
-    //             name: 'Nadmorska visina',
-    //             type: 'coloredarea',
-    //             turboThreshold: 4000,
-    //             data: dataset
-    //             // data: [[0, 150],[0.7, 110],[2.3, 133],[4.56, 50],[5.88, 40]],
-    //             //color: '#35C22E'
-    //         }]
-    //     });
-    // });
+                shared: true,
+                useHTML: true,
+                headerFormat: '<b>{point.key} km</b><br>',
+                pointFormat: '{point.y} mnv',
+                valueDecimals: 2
+            },
+            credits: {
+                enabled: false
+            },
+            plotOptions: {
+                area: {
+                    fillOpacity: 0.1,
+                    connectNulls : true,
+                    marker: {
+                        enabled: false,
+                        symbol: 'circle',
+                        radius: 2,
+                        states: {
+                            hover: {
+                                enabled: true
+                            }
+                        }
+                    }
+                },
+                series: {
+                    cursor: 'pointer',
+                    point: {
+                        events: {
+                            click: function () {
+                                // console.log('Click: ' + (Math.round(this.x * 100) / 100));
+                                document.getElementById("sastavodometar").value = (Math.round(this.x * 100) / 100);
+                                selectedPointOnTrail = pathLineMasterd[this.index];
+                            },
+                            mouseOver: function () {
+                                // console.log('Over: ' + (Math.round(this.x * 100) / 100));
+                                // console.log('Coordinates: ' + pathLine[this.index][0] + ' , ' + pathLine[this.index][1]);
+                                // currentLocSourceObject.geometry.coordinates = [pathLine[this.index][0],pathLine[this.index][1]];
+                                animateMarker([pathLine[this.index][0], pathLine[this.index][1]]);
+                                // if (Number.isInteger(this.index) && pathLine[this.index][0] !== undefined && pathLine[this.index][1] !== undefined) {
+                                // } else {
+                                //     console.log('NO INT');
+                                // }
+                            }
+                        }
+                    }
+                }
+            },
+            series: [{
+                name: 'Nadmorska visina',
+                type: 'coloredarea',
+                turboThreshold: 4000,
+                data: dataset
+                // data: [[0, 150],[0.7, 110],[2.3, 133],[4.56, 50],[5.88, 40]],
+                //color: '#35C22E'
+            }]
+        });
+    });
     
 }
--- examples/js/helper.js
+++ examples/js/helper.js
@@ -25,6 +25,60 @@ function getFeature(featuresCollection, featureType) {
     return returnCollection;
 }
 
+function parseRaw(featuresCollection) {
+  var trail = getTrailLine(featuresCollection);
+  var trailOdometer = parseFloat(turf.lineDistance(trail, 'kilometers')).toFixed(1);
+  
+  var lastCoordinate = trail.geometry.coordinates[getTrailLine(featuresCollection).geometry.coordinates.length - 1];
+  var endPoint = turf.point(lastCoordinate, {
+    'name': 'Kraj',
+    'desc': trailOdometer + '#270#Dosli ste na kraj rute.',
+    'sym': 'Flag, Blue'
+  });
+  featuresCollection.features.push(endPoint);
+
+  var curPoint = {};
+  var prevPoint = {};
+  
+  for (var i = 0; i < trail.geometry.coordinates.length; i++) {
+    var cur = trail.geometry.coordinates[i];
+    curPoint = turf.point([cur[0], cur[1]]);
+    if (i == 0) {
+      cur[3] = 0;
+      cur[4] = 0;
+      cur[5] = 0;
+      cur[6] = 0;
+    } else {
+      var prev = trail.geometry.coordinates[i - 1];
+      prevPoint = turf.point([prev[0], prev[1]]);
+      // odometer
+      var distDelta = turf.distance(prevPoint, curPoint)
+      cur[3] = prev[3] + distDelta;
+      var elevDelta = cur[2] - prev[2];
+      // elevation gain/loss
+      if (elevDelta >= 0) {
+        cur[4] = prev[4] + elevDelta;
+        cur[5] = prev[5];
+      } else {
+        cur[4] = prev[4];
+        cur[5] = prev[5] - elevDelta;
+      }
+      cur[6] = (elevDelta / (distDelta * 1000)) * 100;
+    }
+    prevPoint = JSON.parse(JSON.stringify(curPoint));    
+  }
+
+  var waypoints = getLinePoints(featuresCollection);
+  for (var i = 0; i < waypoints.length; i++) {
+    var parsed = waypoints[i].properties.desc.replace(/(\r\n|\n|\r)/gm,'').split('#');
+    waypoints[i].properties.odometer = parseFloat(parsed[0].replace(',','.'));
+    waypoints[i].properties.pictogram = parsed[1];
+    waypoints[i].properties.desc = parsed[2];
+  }
+
+  return featuresCollection;
+}
+
 // MAP
 
 function addTrailLayers(map) {
@@ -32,26 +86,76 @@ function addTrailLayers(map) {
     'type': 'geojson',
     'data': dataJSON
   });
+
+  map.addSource('temp', {
+    'type': 'geojson',
+    'data': {
+        'type': 'FeatureCollection',
+        'features': []
+      }
+  });
+  
   map.addLayer({
     'id': 'trail-line-outline',
     'type': 'line',
     'source': 'datasource',
     'paint': {
         'line-color': '#C00',
-        'line-width': 6,
-        'line-blur': 2
+        'line-width': 6
     }
   }, 'housenum-label');
+  
   map.addLayer({
     'id': 'trail-line',
     'type': 'line',
     'source': 'datasource',
     'paint': {
         'line-color': '#FFF',
-        'line-width': 3,
-        'line-blur': 1
+        'line-width': 3
     }
   }, 'housenum-label');
+  
+  map.addLayer({
+    'id': 'waypoints-outline-red',
+    'type': 'circle',
+    'source': 'datasource',
+    'paint': {
+        'circle-radius': {
+            'base': 1,
+            'stops': [[4,0],[6,2],[12,9]]
+        },
+        'circle-color': '#C00'
+    },
+    'filter': ['has', 'desc']
+  }, 'housenum-label');
+
+  map.addLayer({
+    'id': 'current-position',
+    'type': 'circle',
+    'source': 'temp',
+    'paint': {
+        'circle-radius': {
+            'base': 1,
+            'stops': [[4,0],[6,1],[12,7]]
+        },
+        'circle-color': '#000'
+    }
+  }, 'housenum-label');
+  
+  map.addLayer({
+    'id': 'waypoints-outline-white',
+    'type': 'circle',
+    'source': 'datasource',
+    'paint': {
+        'circle-radius': {
+            'base': 1,
+            'stops': [[4,0],[6,0.5],[12,5.5]]
+        },
+        'circle-color': '#FFF'
+    },
+    'filter': ['has', 'desc']
+  }, 'housenum-label');
+  
   map.addLayer({
       'id': 'waypoints',
       'type': 'symbol',
@@ -63,7 +167,8 @@ function addTrailLayers(map) {
           'text-color': '#F00'
       },
       'layout': {
-          'icon-image': 'circle-15',
+          'icon-image': 'circle-11',
+          // 'icon-allow-overlap': true,
           'text-field': '{name}',
           'text-anchor': 'left',
           'text-offset': [0.8, 0],
@@ -74,6 +179,12 @@ function addTrailLayers(map) {
       },
       'filter': ['has', 'desc']
   });
+}
+
+function showCurrentPositionOnMap(map, featuresCollection, index) {
+  var trail = getTrailLine(featuresCollection);
+  var newCollection = turf.featureCollection([turf.point([trail.geometry.coordinates[index][0], trail.geometry.coordinates[index][1]])]);
+  map.getSource('temp').setData(newCollection);
 }
 
 // POPUP
@@ -101,16 +212,179 @@ function addPopups(map, popup) {
 }
 
 function generateHTML4popup(o) {
-  var locationName = o.name;
-  var parsed = o.desc.replace(/(\r\n|\n|\r)/gm,'').split('#');
-  var odometer = parseFloat(parsed[0].replace(',','.'));
-  var pictogram = parsed[1];
-  var desc = parsed[2];
-
-  return '<div class="popup-container">'
-            + '<div class="popup-location-name">' + locationName + '</div>'
-            + '<div class="popup-location-odometer">' + odometer + ' km od starta</div>'
-            + '<div class="popup-location-pictogram"><img src="http://www.mtb.ba/wp-content/themes/mtbba-v2/functions/user/raskrsnice/a-ras.php?opis=' + pictogram + '"></div>'
-            + '<div class="popup-location-description">Opis: ' + desc + '</div>'
+  return '<div class="popup">'
+            + '<div class="popup-location-name">' + o.name + '</div>'
+            + '<div class="popup-location-odometer">' + o.odometer + ' km od starta</div>'
+            + '<div class="popup-location-pictogram"><img src="http://www.mtb.ba/wp-content/themes/mtbba-v2/functions/user/raskrsnice/a-ras.php?opis=' + o.pictogram + '"></div>'
+            + '<div class="popup-location-description">Opis: ' + o.desc + '</div>'
         + '</div>';
+}
+
+// CHART
+
+function addChart(map, featuresCollection) {
+  // LINE
+  var dataset = [];
+  var pathLine = getTrailLine(featuresCollection).geometry.coordinates;
+  for (var i = 0; i < pathLine.length; i++) {
+    dataset.push({x: pathLine[i][3], y: pathLine[i][2]});
+  }
+  // console.log(dataset);
+
+  // WPS
+  var plotlines = [];
+  var waypoints = getLinePoints(featuresCollection);
+  for (var i = 0; i < waypoints.length; i++) {
+    plotlines.push({
+        value: waypoints[i].properties.odometer,
+        width: 1,
+        color: '#333',
+        dashStyle: 'dot',
+        label: { 
+            text: waypoints[i].properties.name,
+            rotation: -45,
+            align: 'right',
+            y: -15,
+            x: 2
+        }
+    });
+  }
+
+  Highcharts.chart('elevation-profile', {
+    chart: {
+        type: 'areaspline',
+        pinchType: 'x',
+        zoomType: 'x'
+    },
+    title: {
+        text: window.importovano.trailName
+    },
+    legend: {
+        enabled: false
+    },
+    xAxis: {
+        type: 'integer',
+        title: {
+            text: 'Predjeni put [km]'
+        },
+        plotLines: plotlines,
+        // plotBands: [{ // visualize the weekend
+        //     from: 4.5,
+        //     to: 6.5,
+        //     color: 'rgba(68, 170, 213, .2)'
+        // }]
+    },
+    yAxis: {
+        title: {
+            text: 'Nadmorska visina [m]'
+        },
+        plotLines: [{
+            value: 0,
+            width: 1,
+            color: '#808080'
+        }],
+        min: 0,
+        max: 2000
+    },
+    tooltip: {
+        formatter: function () {
+            return '<b>' + this.x.toFixed(2) + ' km</b><br>' + this.y.toFixed(0) + ' mnv';
+        },
+        shared: true
+    },
+    credits: {
+        enabled: false
+    },
+    plotOptions: {
+        areaspline: {
+            fillOpacity: 0.1,
+            connectNulls : true,
+            marker: {
+                enabled: false,
+                symbol: 'circle',
+                radius: 2,
+                states: {
+                    hover: {
+                        enabled: true
+                    }
+                }
+            }
+        },
+        series: {
+            cursor: 'pointer',
+            point: {
+                events: {
+                    mouseOver: function () {
+                        showCurrentPositionOnMap(map, featuresCollection, this.index);
+                    }
+                }
+            }
+        }
+    },
+    series: [{
+        name: 'Nadmorska visina',
+        turboThreshold: 4000,
+        data: dataset
+    }]
+  });
+
+  var surface = window.importovano.sastav.split('-');
+  var surfaceTypes = ['Asfalt', 'Utaban put', 'Makadam', 'Staza', 'Pl.sekcija'];
+  var surfaceData = [];
+  for (var i = 0; i < 5; i++) {
+    if (surface[i]) {
+      surfaceData.push({
+        name: surfaceTypes[i],
+        y: parseInt(surface[i], 10)
+      });
+    } else {
+      surfaceData.push({
+        name: surfaceTypes[i],
+        y: 0
+      });
+    }
+  }
+
+  Highcharts.chart('surface-pie', {
+        chart: {
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            width: 300,
+            height: 200
+        },
+        title: {
+            text: ''
+        },
+        exporting: {
+          enabled: false,
+        },
+        legend: {
+          align: 'right',
+          verticalAlign: 'middle',
+          layout: 'vertical',
+          itemStyle: {
+              fontSize: '10px'
+          }
+        },
+        tooltip: {
+            pointFormat: '{series.name}: <b>{point.percentage:.1f} %</b>'
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+        series: [{
+            name: 'Podloga',
+            colorByPoint: true,
+            data: surfaceData
+        }]
+    });
 }
