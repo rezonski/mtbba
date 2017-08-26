c97b2f2e091cf5e10baae21d1aeb3188a4a863d1
mersad <mersad.pasic@gmail.com>
2017-08-26 14:04:56 +0200

    progress

--- examples/js/gapi.js
+++ examples/js/gapi.js
@@ -65,47 +65,91 @@ function findStore(w, total, index) {
     replacement: []
   };
   // console.log(setup.endpoint + setup.coordinates + '&radius=' + setup.lvl[0].radius + '&type=' + setup.lvl[0].type + '&key=' + setup.key);
-  $.ajax(setup.endpoint + setup.coordinates + '&radius=' + setup.lvl[0].radius + '&type=' + setup.lvl[0].type + '&key=' + setup.key).done(response => {
-    if (response.status == 'OK') {
-      response.results.forEach(r => {
-        if (window.allIDs.indexOf(r['place_id']) == -1) {
-          window.allIDs.push(r['place_id']);
-          const newStore = turf.point([r.geometry.location.lng, r.geometry.location.lat], {
-            name: r.name,
-            address: r.vicinity 
+  $.ajax(setup.endpoint + setup.coordinates + '&radius=' + setup.lvl[0].radius + '&type=' + setup.lvl[0].type + '&key=' + setup.key).done(placeResponse => {
+    if (placeResponse.status == 'OK') {
+      placeResponse.results.forEach(r => {
+        if (window.stores.allIDs.indexOf(r['place_id']) == -1) {
+          window.stores.allIDs.push(r['place_id']);
+          // Get place details
+          $.ajax('https://maps.googleapis.com/maps/api/place/details/json?placeid=' + r['place_id'] + '&key=' + setup.key).done(detailResponse => {
+            if (detailResponse.status == 'OK') {
+              const det = detailResponse.result;
+              const newStore = turf.point([r.geometry.location.lng, r.geometry.location.lat], {
+                name: det.name,
+                phoneNum: det['international_phone_number'],
+                address: det['formatted_address'],
+                city: det['address_components'][0]['long_name'],
+                openingHours: det['opening_hours'].periods,
+                rating: det.rating,
+                reviews: det.reviews,
+                website: det.website,
+              });
+              window.stores.features.push(newStore);
+            }
           });
-          window.stores.features.push(newStore);
         }
       });
       if (total == (index + 1)) {
-        window.map.addLayer({
-          'id': 'stores',
-          'type': 'symbol',
-          'source': {
-            'type': 'geojson',
-            'data': window.stores
-          },
-          'paint': {
-              'text-halo-color': '#fff',
-              'text-halo-width': 2,
-              'text-halo-blur': 2,
-              'text-color': '#F00'
-          },
-          'layout': {
-              'icon-image': 'circle-11',
-              // 'icon-allow-overlap': true,
-              'text-field': '{name}',
-              // 'text-anchor': 'left',
-              'text-max-width': 5,
-              'text-offset': [0, 1],
-              'text-size': {
-                  'base': 1,
-                  'stops': [[4,0],[6,1],[12,12]]
-              }
-          }
-        });
+        displayStores();
+        localStorage.setItem('stores', JSON.stringify(window.stores));
+        console.info('Saved to local storage');
       }
     }
     console.log(setup.w.properties.name + ' - nearby: ' + JSON.stringify(setup.replacement));
   });
+}
+
+
+function generateStores() {
+  var wps = getLinePoints(dataJSON);
+  wps.forEach((w, wpindex) => {
+      findStore(w, wps.length, wpindex);
+  })
+}
+
+function displayStores() {
+  if (window.map.getSource('stores')) {
+    window.map.getSource('stores').setData()
+  }  
+
+  window.map.addLayer({
+    'id': 'stores',
+    'type': 'symbol',
+    'source': {
+      'type': 'geojson',
+      'data': window.stores
+    },
+    'paint': {
+        'text-halo-color': '#fff',
+        'text-halo-width': 2,
+        'text-halo-blur': 2,
+        'text-color': '#F00'
+    },
+    'layout': {
+        'icon-image': 'circle-11',
+        // 'icon-allow-overlap': true,
+        'text-field': '{name}',
+        // 'text-anchor': 'left',
+        'text-max-width': 5,
+        'text-offset': [0, 1],
+        'text-size': {
+            'base': 1,
+            'stops': [[4,0],[6,1],[12,12]]
+        }
+    }
+  });
+}
+
+function initLocalStorage() {
+  if (localStorage.getItem('stores')) {
+      window.stores = JSON.parse(localStorage.getItem('stores'));
+      displayStores();
+  } else {
+      window.stores = {
+        type: "FeatureCollection",
+        allIDs: [],
+        features: []
+      };
+      generateStores();
+  }
 }
